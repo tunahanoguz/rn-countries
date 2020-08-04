e94eda0aaf1f75321f819d15edce8605834ca221
tunahanoguz <tunahanog@gmail.com>
2020-08-04 18:37:14 +0300

    Add status bar padding to country detail screen

--- src/screens/CountryDetailScreen/index.js
+++ src/screens/CountryDetailScreen/index.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Text, ActivityIndicator } from 'react-native';
-import { ScreenSafeContainer, ScreenHeader } from '../../components';
+import { ScrollView, Text, ActivityIndicator } from 'react-native';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+import {
+  ScreenSafeContainer,
+  ScreenHeader,
+  DetailItem,
+} from '../../components';
 import { fetchSingleCountryByCode } from '../../axiosInstance';
 
 function CountryDetailScreen({ route }) {
@@ -9,17 +14,78 @@ function CountryDetailScreen({ route }) {
 
   const countryCode = route.params.code;
 
+  const statusBarHeight = getStatusBarHeight();
+
   useEffect(() => {
     fetchSingleCountryByCode(countryCode)
       .then((country) => setCountry(country))
       .catch((err) => setError(err));
   }, []);
 
+  function LanguageList() {
+    return country.languages.map((language, index) => (
+      <DetailItem
+        key={`language-${index}`}
+        scoreTitle={`Language ${index + 1}`}
+        scoreDescription={language.name}
+      />
+    ));
+  }
+
+  function CurrencyList() {
+    return country.currencies.map((currency, index) => (
+      <DetailItem
+        key={`currency-${index}`}
+        scoreTitle={`Currency ${index + 1}`}
+        scoreDescription={currency.name}
+      />
+    ));
+  }
+
+  function TimezoneList() {
+    return country.timezones.map((timezone, index) => (
+      <DetailItem
+        key={`timezone-${index}`}
+        scoreTitle={`Timezone ${index + 1}`}
+        scoreDescription={timezone}
+      />
+    ));
+  }
+
+  function BorderList() {
+    return country.borders.map((border, index) => (
+      <DetailItem
+        key={`border-${index}`}
+        scoreTitle={`Border ${index + 1}`}
+        scoreDescription={border}
+      />
+    ));
+  }
+
   if (Object.keys(country).length !== 0) {
     return (
-      <ScreenSafeContainer>
-        <ScreenHeader title={country.name} />
-      </ScreenSafeContainer>
+      <ScrollView style={{ paddingTop: statusBarHeight }}>
+        <ScreenHeader title={country.name} isExistPadding={true} />
+
+        <DetailItem
+          scoreTitle="Native Name"
+          scoreDescription={country.nativeName}
+        />
+        <DetailItem scoreTitle="Capital" scoreDescription={country.capital} />
+        <DetailItem scoreTitle="Region" scoreDescription={country.region} />
+        <DetailItem
+          scoreTitle="Subregion"
+          scoreDescription={country.subregion}
+        />
+
+        <LanguageList />
+
+        <CurrencyList />
+
+        <TimezoneList />
+
+        <BorderList />
+      </ScrollView>
     );
   } else if (error !== '') {
     return (
