4bb6e5c1b90c06c406be5aeec61acc570e51ecb5
tunahanoguz <tunahanog@gmail.com>
2020-08-04 15:05:43 +0300

    Add CountriesScreen to HomeNavigation

--- src/navigations/HomeNavigation/index.js
+++ src/navigations/HomeNavigation/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { HomeScreen } from '../../screens';
+import { HomeScreen, CountriesScreen } from '../../screens';
 
 function HomeNavigation() {
   const Stack = createStackNavigator();
@@ -8,6 +8,7 @@ function HomeNavigation() {
   return (
     <Stack.Navigator initialRouteName="HomeScreen">
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="CountriesScreen" component={CountriesScreen} />
     </Stack.Navigator>
   );
 }
