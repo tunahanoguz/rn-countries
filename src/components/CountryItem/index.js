0f796f3c27ea709d00dce77b593de8d6f765ce5a
tunahanoguz <tunahanog@gmail.com>
2020-08-04 16:15:42 +0300

    Delete unnecessary import from CountryItem component

--- src/components/CountryItem/index.js
+++ src/components/CountryItem/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { TouchableOpacity, View, Text } from 'react-native';
-import { SvgUri } from 'react-native-svg';
 import PropTypes from 'prop-types';
 import styles from './styles';
 
