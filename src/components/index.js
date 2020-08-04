4757cb58502950b9eb9621533cb5f2b0644bbdf1
tunahanoguz <tunahanog@gmail.com>
2020-08-04 16:25:50 +0300

    Add HomeButtonContainer and ScreenHeader components to components barrel file

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
+export HomeButtonContainer from './HomeButtonContainer';
 export HomeButton from './HomeButton';
 export ScreenSafeContainer from './ScreenSafeContainer';
 export CountryItem from './CountryItem';
+export ScreenHeader from './ScreenHeader';
