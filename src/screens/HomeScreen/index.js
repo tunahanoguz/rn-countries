77beba91b2d3c6173317741ca13d10e229a3854a
tunahanoguz <tunahanog@gmail.com>
2020-08-04 14:45:35 +0300

    Add all buttons to HomeScreen
    Redirects will be added later.

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,11 +1,50 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScreenSafeContainer, HomeButton } from '../../components';
+import HomeButtonContainer from '../../components/HomeButtonContainer';
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
+  function goToCountriesScreen() {
+    navigation.navigate('Home', { screen: 'CountriesScreen' });
+  }
+
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <ScreenSafeContainer style={{ justifyContent: 'space-between' }}>
+      <HomeButtonContainer>
+        <HomeButton
+          text="Show countries"
+          firstColor="#667eea"
+          secondColor="#764ba2"
+          func={goToCountriesScreen}
+        />
+      </HomeButtonContainer>
+
+      <HomeButtonContainer>
+        <HomeButton
+          text="Show scores"
+          firstColor="#2af598"
+          secondColor="#009efd"
+          func={goToCountriesScreen}
+        />
+      </HomeButtonContainer>
+
+      <HomeButtonContainer>
+        <HomeButton
+          text="Show ratings"
+          firstColor="#6a11cb"
+          secondColor="#2575fc"
+          func={goToCountriesScreen}
+        />
+      </HomeButtonContainer>
+
+      <HomeButtonContainer>
+        <HomeButton
+          text="Sign Out"
+          firstColor="#ff0844"
+          secondColor="#ffb199"
+          func={goToCountriesScreen}
+        />
+      </HomeButtonContainer>
+    </ScreenSafeContainer>
   );
 }
 
