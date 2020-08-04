35b18183fe1f841adcd08bdfff94bed2ed2331f1
tunahanoguz <tunahanog@gmail.com>
2020-08-04 14:01:20 +0300

    Create home button component

---
+++ src/components/HomeButton/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import PropTypes from 'prop-types';
+import styles from './styles';
+
+function HomeButton({ text, firstColor, secondColor, func }) {
+  return (
+    <LinearGradient
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}
+      colors={[firstColor, secondColor]}>
+      <TouchableOpacity onPress={func}>
+        <Text style={styles.text}>{text}</Text>
+      </TouchableOpacity>
+    </LinearGradient>
+  );
+}
+
+HomeButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  firstColor: PropTypes.string.isRequired,
+  secondColor: PropTypes.string.isRequired,
+  func: PropTypes.func.isRequired,
+};
+
+export default HomeButton;
---
+++ src/components/HomeButton/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'white',
+    fontWeight: 600,
+  },
+});
+
+export default styles;
