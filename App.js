cd5a40209e07d260bf9c5a72b41fd37e82d118dc
tunahanoguz <tunahanog@gmail.com>
2020-08-05 14:56:14 +0300

    Add auth control to App.js

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { TabNavigation } from './src/navigations';
+import auth from '@react-native-firebase/auth';
+import { AuthNavigation, TabNavigation } from './src/navigations';
 
 function App() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    return auth().onAuthStateChanged(onAuthStateChanged);
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <NavigationContainer>
+        <AuthNavigation />
+      </NavigationContainer>
+    );
+  }
+
   return (
     <NavigationContainer>
       <TabNavigation />
