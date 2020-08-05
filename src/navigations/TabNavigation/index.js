70e46325dcc35339b6ff804d8002a6e38095a82d
tunahanoguz <tunahanog@gmail.com>
2020-08-05 17:11:23 +0300

    Add icons to tab navigation

--- src/navigations/TabNavigation/index.js
+++ src/navigations/TabNavigation/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Feather';
 import HomeNavigation from '../HomeNavigation';
 import SettingsNavigation from '../SettingsNavigation';
 
 function TabNavigation() {
   const Tab = createBottomTabNavigator();
 
+  const getTabBarVisibility = (route) => {
+    const routeName = route.state
+      ? route.state.routes[route.state.index].name
+      : '';
+
+    return routeName !== 'QuizScreen';
+  };
+
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeNavigation} />
-      <Tab.Screen name="Settings" component={SettingsNavigation} />
+      <Tab.Screen
+        name="Home"
+        component={HomeNavigation}
+        options={({ route }) => ({
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home" color={color} size={size} />
+          ),
+          tabBarVisible: getTabBarVisibility(route),
+        })}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsNavigation}
+        options={({ route }) => ({
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="settings" color={color} size={size} />
+          ),
+          tabBarVisible: getTabBarVisibility(route),
+        })}
+      />
     </Tab.Navigator>
   );
 }
