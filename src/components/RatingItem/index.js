330f5596a080d8e64051562db061888d37ab3611
tunahanoguz <tunahanog@gmail.com>
2020-08-11 16:04:50 +0300

    Remove some props and navigation func from rating item component

--- src/components/RatingItem/index.js
+++ src/components/RatingItem/index.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import { TouchableOpacity, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import PropTypes from 'prop-types';
-import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 import styles from './styles';
 
-function RatingItem({
-  index,
-  ratingID,
-  playerUsername,
-  totalScore,
-  date,
-  bgColor,
-}) {
-  const navigation = useNavigation();
-
-  function goToRatingDetail() {
-    navigation.navigate('RatingDetailScreen', { id: ratingID, index });
-  }
-
+function RatingItem({ playerUsername, totalScore, date, bgColor }) {
   return (
-    <TouchableOpacity
-      style={[styles.item, { backgroundColor: bgColor }]}
-      onPress={goToRatingDetail}>
+    <View style={[styles.item, { backgroundColor: bgColor }]}>
       <View style={styles.itemTop}>
         <Icon name="user" />
         <Text style={styles.playerUsername}>{playerUsername}</Text>
@@ -39,13 +23,11 @@ function RatingItem({
           <Text>{date}</Text>
         </View>
       </View>
-    </TouchableOpacity>
+    </View>
   );
 }
 
 RatingItem.propTypes = {
-  index: PropTypes.number.isRequired,
-  ratingID: PropTypes.string.isRequired,
   playerUsername: PropTypes.string.isRequired,
   totalScore: PropTypes.number.isRequired,
   date: PropTypes.string.isRequired,
