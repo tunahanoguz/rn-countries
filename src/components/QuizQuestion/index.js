28ca7c512639658c3610ae9b6cf8295583afd3a9
tunahanoguz <tunahanog@gmail.com>
2020-08-11 13:20:10 +0300

    Add new props to quiz question component

--- src/components/QuizQuestion/index.js
+++ src/components/QuizQuestion/index.js
@@ -1,89 +1,137 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import {
+  View,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import styles from './styles';
 
 function QuizQuestion({
+  type,
   question,
   options,
   answer,
-  setAnswer,
   setCurrentQuestion,
-  setCurrentCountry,
+  setCorrectAnswersCount,
 }) {
   const [selectedAnswer, setSelectedAnswer] = useState(5);
   const [isSelected, setIsSelected] = useState(false);
+
+  function saveAnswer(index) {
+    setSelectedAnswer(index);
+    setIsSelected(true);
+  }
+
+  function skipQuestion() {
+    setCurrentQuestion((oldCurrentQuestion) => oldCurrentQuestion + 1);
+    setSelectedAnswer(5);
+    setIsSelected(false);
+  }
+
+  function showSkipAlert(index) {
+    if (answer === index) {
+      setCorrectAnswersCount((count) => count + 1);
+
+      Alert.alert('Correct!', 'You answered the question correctly.', [
+        {
+          text: 'Skip',
+          onPress: () => skipQuestion(),
+        },
+      ]);
+    } else {
+      Alert.alert(
+        'Wrong!',
+        `You answered the question wrongly. Correct answer: ${options[answer]}`,
+        [
+          {
+            text: 'Skip',
+            onPress: () => skipQuestion(),
+          },
+        ],
+      );
+    }
+  }
 
   return (
     <View>
       <Text style={styles.question}>{question}</Text>
 
-      {options.map((option, index) => (
-        <TouchableOpacity
-          key={option.key}
-          style={[
-            styles.optionContainer,
-            (answer === index) === selectedAnswer && {
-              backgroundColor: 'green',
-            },
-          ]}
-          onPress={() => {
-            setAnswer((oldAnswers) => [...oldAnswers, option.key]);
-            setTimeout(
-              () =>
-                setCurrentQuestion(
-                  (oldCurrentQuestion) => oldCurrentQuestion + 1,
-                ),
-              500,
-            );
-            setCurrentCountry(Math.floor(Math.random() * 250));
-            setSelectedAnswer(index);
-            setIsSelected(true);
-          }}>
-          <Text>{option.text}</Text>
-        </TouchableOpacity>
-      ))}
-
-      {isSelected &&
-        (answer === selectedAnswer ? (
-          <View
-            style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginTop: 10,
-              padding: 30,
-              backgroundColor: 'green',
+      <ScrollView>
+        {options.map((option, index) => (
+          <TouchableOpacity
+            key={index}
+            style={[
+              styles.optionContainer,
+              (answer === index) === selectedAnswer && {
+                backgroundColor: 'green',
+              },
+            ]}
+            onPress={() => {
+              saveAnswer(index);
+              showSkipAlert(index);
             }}>
-            <Text style={{ color: 'white' }}>Correct!</Text>
-          </View>
-        ) : (
-          <View
-            style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginTop: 10,
-              padding: 30,
-              backgroundColor: 'red',
-            }}>
-            <Text style={{ color: 'white' }}>Wrong!</Text>
-          </View>
+            {type === 0 ? (
+              <Text>{option.length !== 0 ? option : 'None'}</Text>
+            ) : (
+              <Image
+                source={{
+                  uri: option,
+                }}
+                style={{
+                  alignSelf: 'center',
+                  width: 128,
+                  height: 128,
+                  marginTop: -30,
+                }}
+              />
+            )}
+          </TouchableOpacity>
         ))}
+      </ScrollView>
+
+      {/*{isSelected &&*/}
+      {/*  (answer === selectedAnswer ? (*/}
+      {/*    <View*/}
+      {/*      style={{*/}
+      {/*        justifyContent: 'center',*/}
+      {/*        alignItems: 'center',*/}
+      {/*        marginTop: 10,*/}
+      {/*        padding: 30,*/}
+      {/*        backgroundColor: 'green',*/}
+      {/*      }}>*/}
+      {/*      <Text style={{ color: 'white' }}>Correct!</Text>*/}
+      {/*    </View>*/}
+      {/*  ) : (*/}
+      {/*    <View*/}
+      {/*      style={{*/}
+      {/*        justifyContent: 'center',*/}
+      {/*        alignItems: 'center',*/}
+      {/*        marginTop: 10,*/}
+      {/*        padding: 30,*/}
+      {/*        backgroundColor: 'red',*/}
+      {/*      }}>*/}
+      {/*      <Text style={{ color: 'white' }}>Wrong!</Text>*/}
+      {/*    </View>*/}
+      {/*  ))}*/}
     </View>
   );
 }
 
 QuizQuestion.propTypes = {
+  type: PropTypes.number.isRequired,
   question: PropTypes.string.isRequired,
-  options: PropTypes.arrayOf(
-    PropTypes.shape({
-      key: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-    }),
-  ),
+  options: PropTypes.array.isRequired,
   answer: PropTypes.number.isRequired,
-  setAnswer: PropTypes.func.isRequired,
   setCurrentQuestion: PropTypes.func.isRequired,
-  setCurrentCountry: PropTypes.func.isRequired,
+  setCorrectAnswersCount: PropTypes.func.isRequired,
+};
+
+QuizQuestion.defaultProps = {
+  type: 0,
 };
 
 export default QuizQuestion;
