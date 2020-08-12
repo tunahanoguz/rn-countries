c8c41f06b7bad24b89c36bfcdb27eccc5281cd26
tunahanoguz <tunahanog@gmail.com>
2020-08-12 09:51:49 +0300

    Replace TouchableOpacity with View in score item component

--- src/components/ScoreItem/index.js
+++ src/components/ScoreItem/index.js
@@ -1,28 +1,21 @@
 import React from 'react';
-import { TouchableOpacity, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import PropTypes from 'prop-types';
-import { useNavigation } from '@react-navigation/native';
 import moment from 'moment';
 import styles from './styles';
 
 function ScoreItem({ scoreID, scoreType, totalScore, date }) {
-  const navigation = useNavigation();
-
-  function goToScoreDetail() {
-    navigation.navigate('ScoreDetailScreen', { id: scoreID, scoreType });
-  }
-
   const trimmedDate = moment(date).format('LL');
 
   return (
-    <TouchableOpacity style={styles.item} onPress={goToScoreDetail}>
+    <View style={styles.item}>
       <View style={styles.leftScoreItem}>
         <Text>Total Score</Text>
         <Text style={styles.scoreText}>{totalScore}</Text>
       </View>
 
       <Text style={styles.dateText}>{trimmedDate}</Text>
-    </TouchableOpacity>
+    </View>
   );
 }
 
